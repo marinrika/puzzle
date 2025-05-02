e7cbbef283316d5179208cce0650c8c18c0dfd06
marinrika <marinrika2016@gmail.com>
2025-05-02 12:36:17 +0300

    change

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import MainPage from './pages/main-page/main-page';
 import LoginPage from './pages/login-page/login-page';
 import StartPage from './pages/start-page/start-page';
-// import NotFoundPage from './pages/not-found-page/not-found-page';
+import NotFoundPage from './pages/not-found-page/not-found-page';
 import './App.css';
 import MainLayout from './components/layouts/MainLayout';
 
@@ -16,10 +16,10 @@ function App() {
       <div className="App">
         <Routes>
           <Route path="/" element={<MainLayout />}>
-            {/* <Route path="*" element={<NotFoundPage />} /> */}
             <Route path="/" element={<MainPage />} />
             <Route index path="login" element={<LoginPage />} />
             <Route path="start" element={<StartPage />} />
+            <Route path="*" element={<NotFoundPage />} />
           </Route>
         </Routes>
       </div>
